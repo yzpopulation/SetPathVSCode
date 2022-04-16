d42307a90918a2fa08bfbdfa145615478bf9569d
admin <admin@admin.com>
2022-04-16 19:06:35 +0800

    fix

--- main.go
+++ main.go
@@ -21,16 +21,16 @@ func main() {
 		return
 	}
 	SetPath("*\\shell\\VSCode")
-	SetPath("Directory\\Background\\shell")
-	SetPath("Directory\\shell")
-	SetPath("Drive\\shell")
+	SetPath("Directory\\Background\\shell\\VSCode")
+	SetPath("Directory\\shell\\VSCode")
+	SetPath("Drive\\shell\\VSCode")
 }
 func SetPath(basePath string) {
 	key, exist, err := registry.CreateKey(registry.CURRENT_USER, "SOFTWARE\\Classes\\"+basePath, registry.ALL_ACCESS)
-	defer key.Close()
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer key.Close()
 	if exist {
 		fmt.Println(`键已存在`)
 	} else {
@@ -38,17 +38,18 @@ func SetPath(basePath string) {
 		key.SetExpandStringValue("", "通过 Code 打开")
 		key.SetExpandStringValue("Icon", CodePath)
 		key2, exist2, err := registry.CreateKey(registry.CURRENT_USER, "SOFTWARE\\Classes\\"+basePath+"\\command", registry.ALL_ACCESS)
-		defer key2.Close()
 		if err != nil {
 			fmt.Println(err)
 		}
+		defer key2.Close()
 		if exist2 {
 			fmt.Println(`command键已存在`)
 		} else {
 			fmt.Println(`新建注册表command键已存在键`)
-			key2.SetExpandStringValue("", "\""+CodePath+"\" \"%1\"")
+			key2.SetExpandStringValue("", "\""+CodePath+"\" \"%V\"")
 		}
 	}
+
 }
 func PathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
